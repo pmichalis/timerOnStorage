85fcf70aba7cd6a29e547d24bdcd1bb5105d5599
pmichalis <piotrmichalis@gmail.com>
2019-02-28 22:49:20 +0100

    PM
    working

---
+++ scrypt.js
@@ -0,0 +1,49 @@
+
+	 let status = 0;
+	 let time = 0;
+	 
+	 function start(){
+		status = 1;
+		document.getElementById("startBtn").disabled = true;
+		timer();
+		
+	 }
+	 function stop(){
+		status = 0;
+		document.getElementById("startBtn").disabled = false;
+
+	 }
+	 function reset(){
+		status = 0;
+		time = 0;
+		document.getElementById("startBtn").disabled = false;
+		document.getElementById("timelabel").innerHTML = "00:00";
+
+	 }
+	 function timer(){
+	 
+		if (status == 1 ){
+			setTimeout(function(){
+			time++;
+				let min = Math.floor(time/100/60);
+				let sec = Math.floor(time/100);
+				if (min < 10 ) {
+					min = "0" + min;
+				}
+				if (sec >= 60 ){
+					sec = sec % 60;
+				}
+				if (sec < 10) {
+					sec = "0" + sec;
+				}
+				document.getElementById("timeLabel").innerHTML = min + ":" + sec;
+				timer();
+				localStorage.setItem('timer', JSON.stringify(time));
+				const retrievedObject = localStorage.getItem('timer');
+				localStorage.removeItem('reset');
+				console.log(localStorage.getItem('reset'));
+			},10);
+		}
+	 
+	 }
+	
